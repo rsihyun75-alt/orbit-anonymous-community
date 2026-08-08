6c381d8be177bbfb87102a17e4746b73a7784089
rsihyun75-alt <rsihyun75@gmail.com>
2026-08-08 23:04:39 +0900

    Build anonymous community app

---
+++ app/posts/[id]/page.tsx
@@ -0,0 +1,12 @@
+import CommunityApp from "../../components/community-app";
+
+type PageProps = {
+  params: Promise<{ id: string }>;
+};
+
+export default async function PostPage({ params }: PageProps) {
+  const { id } = await params;
+  const postId = Number(id);
+  return <CommunityApp initialPostId={Number.isInteger(postId) ? postId : -1} />;
+}
+
